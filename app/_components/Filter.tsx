061b64193350a7612dbddde3ec09ea01b5c5d97a
williford-abunas <abunaswilliford@gmail.com>
2025-09-16 20:17:27 +1200

    resized LoginMessage to full

--- app/_components/Filter.tsx
+++ app/_components/Filter.tsx
@@ -2,6 +2,30 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+function Button({
+  children,
+  handleFilter,
+  filter,
+  activeFilter,
+}: ButtonProps) {
+  return (
+    <button
+      onClick={() => handleFilter(filter)}
+      className={`py-2 px-4 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
+
+interface ButtonProps {
+  children: React.ReactNode;
+  handleFilter: (filter: string) => void;
+  filter: string;
+  activeFilter: string;
+}
 
 export default function Filter() {
   const searchParams = useSearchParams();
@@ -9,28 +33,39 @@ export default function Filter() {
   const pathname = usePathname();
 
   const activeFilter = searchParams.get("capacity") ?? "all";
-  
-function handleFilter(filter: string) {
-  const params = new URLSearchParams(searchParams);
-  params.set("capacity", filter);
-  router.push(`${pathname}?${params.toString()}`, { scroll: false });
-}
 
-  return <div className="border border-primary-800 flex">
-    <Button handleFilter={handleFilter} filter="all" activeFilter={activeFilter}>All Cabins</Button>
-    <Button handleFilter={handleFilter} filter="small" activeFilter={activeFilter}>1&ndash;3 Guests</Button>
-    <Button handleFilter={handleFilter} filter="medium" activeFilter={activeFilter}>4&ndash;7 Guests</Button>
-    <Button handleFilter={handleFilter} filter="large" activeFilter={activeFilter}>8&ndash;12 Guests</Button>
-  </div>;
-}
+  function handleFilter(filter: string) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  }
 
-interface ButtonProps {
-  children: React.ReactNode,
-  handleFilter: (filter: string) => void,
-  filter: string,
-  activeFilter: string,
-}
-
-function Button({children, handleFilter, filter, activeFilter}: ButtonProps) {
-  return <button onClick={() => handleFilter(filter)} className={`py-2 px-4 hover:bg-primary-700 ${filter === activeFilter ? "bg-primary-700 text-primary-50" : ""}`}>{children}</button>;
+  return (
+    <div className="border border-primary-800 flex">
+      <Button handleFilter={handleFilter} filter="all" activeFilter={activeFilter}>
+        All Cabins
+      </Button>
+      <Button
+        handleFilter={handleFilter}
+        filter="small"
+        activeFilter={activeFilter}
+      >
+        1&ndash;3 Guests
+      </Button>
+      <Button
+        handleFilter={handleFilter}
+        filter="medium"
+        activeFilter={activeFilter}
+      >
+        4&ndash;7 Guests
+      </Button>
+      <Button
+        handleFilter={handleFilter}
+        filter="large"
+        activeFilter={activeFilter}
+      >
+        8&ndash;12 Guests
+      </Button>
+    </div>
+  );
 }
--- app/_components/LoginMessage.tsx
+++ app/_components/LoginMessage.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function LoginMessage() {
   return (
-    <div className='grid bg-primary-800 '>
+    <div className='grid bg-primary-800 h-full w-full'>
       <p className='text-center text-xl py-12 self-center'>
         Please{' '}
         <Link href='/login' className='underline text-accent-500'>
