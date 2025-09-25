b9255bff2bd29cf0d4580ed99c5d144af9f35f04
williford-abunas <abunaswilliford@gmail.com>
2025-09-25 20:32:22 +1200

    copy loading page to accounts folder

---
+++ app/account/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loader() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
